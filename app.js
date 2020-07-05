45076fd363073b90c017a22ace78d6201f567204
Glitch (whoami-api-orara) <none>
2020-07-05 00:45:42 +0000

    🌖💪 Updated with Glitch

--- app.js
+++ app.js
@@ -5,36 +5,15 @@ app.get("/test", function(req, res) {
   res.send("Test method is connected...");
 });
 
-const basePath = "/api/timestamp/";
-//Get input from client with Route parameters
-const endpoint = `${basePath}:date_string`;
-//Mount the date method to the endpoint
-app.get(endpoint, function(req, res) {
-  console.log(typeof req.params.date_string, req.params.date_string);
-  res.json(dateParser(req.params.date_string));
-});
+app.get("/api/whoami", headerInfo);
 
-app.get(basePath, function(req, res) {
-  res.json(dateParser(Date.now()));
-});
-
-function dateParser(time) {
-  let date = new Date(time);
-
-  if (isInvalid(date)) {
-    date = new Date(Number(time));
-    if (isInvalid(date)) {
-      return { error: "Invalid Date" };
-    }
-  }
-  return {
-    unix: date.getTime(),
-    utc: date.toUTCString()
+function headerInfo(req, res) {
+  const obj = {
+    ipaddress: req.headers["x-forwarded-for"].split(",")[0],
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
   };
-}
-
-function isInvalid(date) {
-  return date.toString() === "Invalid Date";
+  res.json(obj);
 }
 
 module.exports = app;
